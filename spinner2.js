98c555c07ad8ef855e80e151f829d8ecd74b55e0
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-08-25 19:50:32 +0000

    eslint --fix on spinner

--- spinner2.js
+++ spinner2.js
@@ -1,26 +1,26 @@
 process.stdout.write('hello from spinner1.js... \rheyyy\n');
 //set a global timer
-time = 0;
+let time = 0;
 //loop through an object containing all of our states
 const spinnerState = ([
-  '\r|  ', 
-  '\r/  ', 
-  '\r-  ', 
-  '\r\\   ', 
-  '\r|  ', 
-  '\r/  ', 
-  '\r-  ', 
-  '\r\\  ', 
+  '\r|  ',
+  '\r/  ',
+  '\r-  ',
+  '\r\\   ',
+  '\r|  ',
+  '\r/  ',
+  '\r-  ',
+  '\r\\  ',
   '\r|   '
-])
+]);
 
 //  console.log(spinnerState)
 
-for(const state in spinnerState){
-   setTimeout(() => {
-process.stdout.write(spinnerState[state])
-//increment our timer so it doesn't all appear at once
-}, time += 100)
+for (const state in spinnerState) {
+  setTimeout(() => {
+    process.stdout.write(spinnerState[state]);
+    //increment our timer so it doesn't all appear at once
+  }, time += 100);
 }
 // setTimeout(() => {
 //   process.stdout.write('\r|   ');
@@ -36,27 +36,27 @@ process.stdout.write(spinnerState[state])
 
 // setTimeout(() => {
 //   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\\   '); 
+//   process.stdout.write('\r\\   ');
 // }, 700);
 
 // // ... fill in the rest yourself ...
 
 // setTimeout(() => {
 //   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\|   '); 
+//   process.stdout.write('\r\|   ');
 // }, 900);
 
 // setTimeout(() => {
 //   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\/   '); 
+//   process.stdout.write('\r\/   ');
 // }, 1100);
 
 // setTimeout(() => {
 //   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r-   '); 
+//   process.stdout.write('\r-   ');
 // }, 1300);
 
 // setTimeout(() => {
 //   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\\   '); 
+//   process.stdout.write('\r\\   ');
 // }, 1500);
